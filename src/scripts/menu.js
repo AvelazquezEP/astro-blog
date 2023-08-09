a6444600a2613608e5d18d0ad970505c4c034301
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-08-09 09:56:46 -0700

    UPDATE: the index and component, now we use a base layout in this way we can have a main layout and only update the things we need

---
+++ src/scripts/menu.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+    // document.querySelector('.nav-menu').classList.toggle('expanded');
+    document.querySelector('.nav-links').classList.toggle('expanded');
+});
